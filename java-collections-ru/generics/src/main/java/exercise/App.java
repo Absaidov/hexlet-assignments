42f99d2995e9c80f0d31e5b6d250c9a607a21ba3
@hexlet/cli <support@hexlet.io>
2023-03-06 12:19:02 +0300

    submit java-collections-ru/generics

---
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -0,0 +1,60 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
+// BEGIN
+class App {
+    public static void main(String[] args) {
+        List<Map<String, String>> books = new ArrayList<>();
+
+        Map<String, String> book1 = new HashMap<>(
+                Map.of("title", "Cymbeline", "author", "Shakespeare", "year", "1611")
+        );
+        Map<String, String> book2 = new HashMap<>(
+                Map.of("title", "Book of Fooos", "author", "FooBar", "year", "1111")
+        );
+        Map<String, String> book3 = new HashMap<>(
+                Map.of("title", "The Tempest", "author", "Shakespeare", "year", "1611")
+        );
+        Map<String, String> book4 = new HashMap<>(
+                Map.of("title", "Book of Foos Barrrs", "author", "FooBar", "year", "2222")
+        );
+        Map<String, String> book5 = new HashMap<>(
+                Map.of("title", "Still foooing", "author", "FooBar", "year", "3333")
+        );
+
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+        books.add(book5);
+        Map<String, String> where = new HashMap<>(Map.of("author", "Shakespeare", "year", "1611"));
+        List<Map<String, String>> result = App.findWhere(books, where);
+        System.out.println(result);
+    }
+    public static List<Map<String, String>> findWhere(List<Map<String, String>> data, Map<String, String> items) {
+        List<Map<String, String>> result = new ArrayList<>();
+
+        for (Map<String, String> item: data) {
+            boolean find = true;
+
+            for (Entry<String, String> entry: items.entrySet()) {
+                String itemValue = item.getOrDefault(entry.getKey(), "");
+                if (!itemValue.equals(entry.getValue())) {
+                    find = false;
+                }
+            }
+
+            if (find) {
+                result.add(item);
+            }
+        }
+
+        return result;
+    }
+}
+//END
