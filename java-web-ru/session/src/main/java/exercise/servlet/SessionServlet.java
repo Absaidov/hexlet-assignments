d226f9bfa12af8698cfc1f16a118e055b1a8e9c4
@hexlet/cli <support@hexlet.io>
2023-06-24 15:39:40 +0300

    submit java-web-ru/session

--- java-web-ru/session/src/main/java/exercise/servlet/SessionServlet.java
+++ java-web-ru/session/src/main/java/exercise/servlet/SessionServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.RequestDispatcher;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static exercise.App.getUsers;
